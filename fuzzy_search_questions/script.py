14a7906913f437451a5fc17b650855ed76dd8d89
Yadu Raj Bhageria <yaduraj1993@gmail.com>
2020-09-25 11:18:53 +0530

    Cleaned up script.py and added comments explaining the code where necessary

--- fuzzy_search_questions/script.py
+++ fuzzy_search_questions/script.py
@@ -2,15 +2,17 @@ import pandas as pd
 from fuzzywuzzy import process
 from numpy import random
 
-
+# Variable to control printing to console during the script
+display = True
+# Load data on players and teams with official team names
 players = pd.read_csv('data_players.csv')
 teams = pd.read_csv('data_teams.csv')
 teams_official = pd.read_csv('data_teams_official.csv')
+# Load question data and drop rows with incomplete information
 qs = pd.read_csv('data_questions.csv')
-
-# print(questions)
 qs.dropna(inplace=True)
-# print(qs)
+
+# Initalize empty columns for storing information
 qs['Answer Match'] = ""
 qs['Answer Match Certainty'] = ""
 qs['Answer Type'] = ""
@@ -18,15 +20,16 @@ qs['Option 1'] = ""
 qs['Option 2'] = ""
 qs['Option 3'] = ""
 
-display = True
-
+# Iterate over all remaining rows
 for i in qs.index:
     ans = qs.loc[i, 'Answer']
+    # Fuzzy search to find highest match in players data
     ratio_players = process.extract( ans, players['Players'].values)
     max_player_ratio = ratio_players[0]
+    # Fuzzy search to find highest match in teams data
     ratio_teams = process.extract( ans, teams['Teams'].values)
     max_team_ratio = ratio_teams[0]
-
+    # Take value that has the least Levenshtein distance, i.e. highest ratio
     max_ratio = max(max_player_ratio[1], max_team_ratio[1])
     qs.loc[i, 'Answer Match Certainty'] = max_ratio
     max_result = (max_player_ratio[0] if max_ratio == max_player_ratio[1] else max_team_ratio[0])
@@ -38,6 +41,7 @@ for i in qs.index:
         print('Match: ', max_result)
         print('Certainty: ', max_ratio)
 
+    # Set data to sample from given what answer matched to (players or teams)
     if ( max_ratio == max_player_ratio[1] ):
         qs.loc[i, 'Answer Type'] = 'Player'
         # answer_type = 'Player'
@@ -49,6 +53,7 @@ for i in qs.index:
         team_number = teams.loc[mask_team, 'Team Number'].values[0]
         wrong_choices = teams_official.loc[teams_official['Team Number'] != team_number, 'Teams'].values
 
+    # Sample 3 choices from the wrong choices without replacement
     wrong_answers = random.choice(wrong_choices, 3, replace = False)
     qs.loc[i, 'Option 1'] = wrong_answers[0]
     qs.loc[i, 'Option 2'] = wrong_answers[1]
